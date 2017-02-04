4ff8f66d7d40959857eb7121cfc6aee2fb310216
Fran Saavedra <fran14.95@gmail.com>
2017-02-04 20:48:12 +0100

    Añadidos algunos textos y cambiados mensajes de error de reservas

--- js/jqBootstrapValidation.js
+++ js/jqBootstrapValidation.js
@@ -836,7 +836,7 @@
 			required: {
 				name: "Required",
 				type: "required",
-				message: "This is required<!-- data-validator-required-message to override -->"
+				message: "Complete este campo<!-- data-validator-required-message to override -->"
 			},
 			checkone: {
 				name: "Checkone",
