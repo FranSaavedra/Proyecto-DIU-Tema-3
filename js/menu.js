7082c52219e8171262e615e82d968b1761cdb4f5
Francisco Javier Calvo <franciscojaviercalvoserrano@gmail.com>
2017-02-14 10:17:11 +0100

    Modificacíon de los enlaces en el Menú

--- js/menu.js
+++ js/menu.js
@@ -4,39 +4,68 @@ $("document").ready(function(){
     var entrante = $("#entrante");
     var entrante_items = $(".entr");
     entrante_items.hide();
+    
     entrante.click(function(){
         entrante_items.toggle();
+    });
+    
+    entrante.hover(function(){
+       entrante.css("cursor",["pointer"]) 
     });
     
     /*ocultación/mostrar elementos de los Primeros*/
     var primeros = $("#primeros");
     var primeros_items = $(".prim");
     primeros_items.hide();
+    
     primeros.click(function(){
         primeros_items.toggle();
     });
+    
+    primeros.hover(function(){
+       primeros.css("cursor",["pointer"]) 
+    });
+    
     
     /*ocultación/mostrar elementos de los Segundos*/
     var segundos = $("#segundos");
     var segundo_items = $(".seg");
     segundo_items.hide();
+    
     segundos.click(function(){
         segundo_items.toggle();
     });
+    
+    segundos.hover(function(){
+       segundos.css("cursor",["pointer"]) 
+    });
+    
     
     /*ocultación/mostrar elementos de los Postres*/
     var postres = $("#postres");
     var postre_items = $(".postr");
     postre_items.hide();
+    
     postres.click(function(){
         postre_items.toggle();
     });
+    
+    postres.hover(function(){
+       postres.css("cursor",["pointer"]) 
+    });
+    
     
     /*ocultación/mostrar elementos de los Bebidas*/
     var bebidas = $("#bebidas");
     var bebidas_items = $(".bebi");
     bebidas_items.hide();
+    
     bebidas.click(function(){
         bebidas_items.toggle();
     });
+    
+    bebidas.hover(function(){
+       bebidas.css("cursor",["pointer"]) 
+    });
+    
 });
